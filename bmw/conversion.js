d598f90c0f0c73b70f722972d3050acdc43a7387
Popkidtech <popkidgeee@gmail.com>
2024-08-24 12:31:09 +0300

    Update conversion.js

--- bmw/conversion.js
+++ bmw/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"BMW-Md" ,
+      pack:"POPKID-Md" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
