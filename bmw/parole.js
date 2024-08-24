3b39ffab5973fb8572ddf2a33c05d9d4fa33da9b
Popkidtech <popkidgeee@gmail.com>
2024-08-24 12:56:18 +0300

    Update parole.js

--- bmw/parole.js
+++ bmw/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------Bmw-lyrics-finder--------
+    let msg = `---------popkid-lyrics-finder--------
 
 * *Artist :* ${lyrics.artist}
 
